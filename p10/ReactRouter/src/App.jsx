cca20875ba8d1062530481e9fd059e6360d623c7
Patade-Soham <sohampatade30@eng.rizvi.edu.in>
2026-06-23 02:28:04 +0530

    Brace for messii

--- p10/ReactRouter/src/App.jsx
+++ p10/ReactRouter/src/App.jsx
@@ -4,6 +4,10 @@ import Home from './components/Home';
 import About from './components/About';
 import Dashboard from './components/Dashboard';
 import Navbar from './components/Navbar';
+import ParamComo from './components/ParamComo';
+import Courses from './components/Courses';
+import MockTest from './components/MockTest';
+import Reports from './components/Reports';
 
 const router = createBrowserRouter(
   [
@@ -22,7 +26,30 @@ const router = createBrowserRouter(
       element: <div> 
             <Navbar/>
         <Dashboard/>
-        </div>}
+        
+        </div>,
+        children:[
+          {path:"courses",
+            element:<Courses/>
+          },
+          {
+            path:'mocks-tests',
+            element:<MockTest/>
+          },
+          {
+            path:"reports",
+            element:<Reports/>
+          },
+        ]
+        },
+    {
+      path:"/student/:id",
+      element:
+      <div>
+        <Navbar/>
+        <ParamComo/>
+      </div>
+    }
   ]
 )  ;
 function App() {
